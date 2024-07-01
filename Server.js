f525b4f569492f66e8fb3d96fffb2d860ab174bb
Nalenhle <nalenhlendlovu@gmail.com>
2024-07-01 21:59:34 +0200

    finish node api version 2

--- Server.js
+++ Server.js
@@ -1,12 +1,48 @@
+require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
+const productRoute = require('./routes/productRoute');
+const errorMiddleware = require('./middleware/errorMiddleware')
+var cors = require('cors')
+
+
 const app = express()
 
+const PORT = process.env.PORT || 4000
+const MONGO_URL = process.env.MONGO_URL
+
+var corsOptions = {
+    origin: 'http://example.com',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+
+
 //routes
+
+app.use('/api/products', productRoute)
+
 
 app.get('/', (req, res) =>{
     res.send('Hie Node API')
 })
+app.get('/blog', (req, res) =>{
+    res.send('Hie blog, My name is Nalenhle')
+})
 
-app.listen(4000, ()=> {
-    console.log('Node API app is running on port 4000')
+app.use(errorMiddleware);
+
+mongoose.
+connect(MONGO_URL)
+.then(() => {
+    console.log('connected to MongoDB')
+    app.listen(PORT, ()=> {
+        console.log('Node API app is running on port ${PORT}')
+    });
+}).catch((error) => {
+    console.log(error)
 })
